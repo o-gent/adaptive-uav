1a9ad2b218985c48010d9414b201ff853ce18b12
o-gent <olivergent@btinternet.com>
2021-11-28 00:18:01 +0000

    introducing IMU stuff,  components/libs readme

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Adaptive UAV
 
-Some things used for my Aerospace MEng individual project.
+ESP32 program to control/log a micro UAV as part of my Aerospace MEng individual project.
 
-## libraries/components used
+## Purpose
+- sync actions and data with catapult launch 
+- Actuate servos at precise points during flight
+- Potentially control pitch
+- record/send IMU data -> ultimately to get velocities etc
 
-- IMU: https://github.com/adafruit/Adafruit_BNO055
+## components used
 - board: https://wiki.dfrobot.com/FireBeetle_Board_ESP32_E_SKU_DFR0654
+- IMU: https://www.adafruit.com/product/2472
+
+## libraries used
+- IMU: https://github.com/adafruit/Adafruit_BNO055
+- Servo control: https://github.com/madhephaestus/ESP32Servo
+- WebSerial: https://github.com/ayushsharma82/WebSerial
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,96 @@
 #include <Arduino.h>
-#include <Dynamixel2Arduino.h>
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
+#include <ESP32Servo.h>
 #include <Adafruit_BNO055.h>
+#include <WebSerial.h>
+
+// dependancies
+#include <Adafruit_Sensor.h>
 #include <utility/imumaths.h>
 #include <SPI.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <WebSerial.h>
+
+/* 
+    Webserial setup
+*/
 
 AsyncWebServer server(80);
-const char* ssid = "SKY938BB";    // Your WiFi SSID
-const char* password = "FNLBCPFYBY";  // yes i realise this is public :) Your WiFi Password
+const char* ssid = "DESKTOP-2J7JM8Q 6736";    // Your WiFi SSID
+const char* password = "gentnet69"; 
+
+/*
+    Servo setup
+*/
+
+Servo dihedral_servo;
+int dihedral_pin = 13;
+int dihedral_pos = 0;
+
+Servo sweep_servo;
+int dihedral_pin = 13;
+int dihedral_pos = 0;
+
+/*
+    IMU setup
+*/
+
+// Set the delay between fresh samples
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 10;
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+//velocity = accel*dt (dt in seconds)
+//position = 0.5*accel*dt^2
+
+/*
+    functions
+*/
 
 /* Message callback of WebSerial */
-void recvMsg(uint8_t *data, size_t len){
-  WebSerial.println("Received Data...");
-  String d = "";
-  for(int i=0; i < len; i++){
-    d += char(data[i]);
-  }
-  WebSerial.println(d);
+void recvMsg(uint8_t *data, size_t len) {
+    WebSerial.println("Received Data...");
+    String d = "";
+    for(int i=0; i < len; i++) {
+        d += char(data[i]);
+    }
+    WebSerial.println(d);
 }
 
+/*
+    Main Loop
+*/
+
 void setup() {
-    // put your setup code here, to run once:
+    /*
+        General
+    */
     Serial.begin(9600);
 
+    /*
+        IMU
+    */
+    // Initialise the sensor
+    if(!bno.begin())
+    {
+        /* There was a problem detecting the BNO055 ... check your connections */
+        WebSerial.println("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        while(1);
+    }
+
+    bno.setExtCrystalUse(true);
+
+    /* 
+        Servo
+    */
+    dihedral_servo.attach(dihedral_pin);
+
+    /*
+        Webserial
+    */
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
-    
     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-      Serial.printf("WiFi Failed!\n");
-      return;
+        Serial.printf("WiFi Failed!\n");
+        return;
     }
-    
-    
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
     // WebSerial is accessible at "<IP Address>/webserial" in browser
@@ -46,9 +101,25 @@ void setup() {
 
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    // WebSerial.println("Hello!");
-    sleep(1);
+    unsigned long tStart = micros();
+
+    // Get IMU data
+    sensors_event_t event;
+    bno.getEvent(&event);
+
+    WebSerial.println((float)event.orientation.x);
+    WebSerial.println((float)event.orientation.y);
+    WebSerial.println((float)event.orientation.z);
+
+    sensors_event_t orientationData , accelData, gyroData;
+    
+    bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+    bno.getEvent(&gyroData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+    bno.getEvent(&accelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+
     WebSerial.println("hello");
 
+    while ((micros() - tStart) < (BNO055_SAMPLERATE_DELAY_MS * 1000)) {
+        //poll until the next sample is ready
+    }
 }
