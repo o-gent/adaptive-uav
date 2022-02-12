8f971a432477a2f9ac51f5db5a53b1fb0ff55116
o-gent <olivergent@btinternet.com>
2022-02-12 16:40:58 +0000

    sockets

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,37 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 #include <Adafruit_BNO055.h>
-#include <WebSerial.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
 
 // dependancies
 #include <Adafruit_Sensor.h>
 #include <utility/imumaths.h>
 #include <SPI.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
+
+/*
+    General
+*/
+
+unsigned long allTime = 0; // keep track of how long the UAV is in the air for
+unsigned long tStart = 0; // keep track of loop time
 
 /* 
     Webserial setup
 */
 
 AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
 const char* ssid = "DESKTOP-2J7JM8Q 6736";    // Your WiFi SSID
-const char* password = "gentnet69"; 
+const char* password = "gentnet69";
 
 /*
     Servo setup
 */
 
 Servo dihedral_servo;
-int dihedral_pin = 13;
+int dihedral_pin = 25;
 int dihedral_pos = 0;
 
 Servo sweep_servo;
@@ -43,16 +51,53 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 /*
     functions
 */
-
-/* Message callback of WebSerial */
-void recvMsg(uint8_t *data, size_t len) {
-    WebSerial.println("Received Data...");
-    String d = "";
-    for(int i=0; i < len; i++) {
-        d += char(data[i]);
-    }
-    WebSerial.println(d);
+char result[8];
+char* convert(float var) {
+    return dtostrf(var, 6, 2, result);
 }
+
+char* sensor_data(char * data) {
+    // Get IMU data
+    sensors_event_t event;
+    bno.getEvent(&event);
+
+    //client->text(convert((float)event.orientation.x));
+    //client->text(convert((float)event.orientation.y));
+    //client->text(convert((float)event.orientation.z));
+
+    // sensors_event_t orientationData , accelData, gyroData;
+    
+    // bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+    // bno.getEvent(&gyroData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+    // bno.getEvent(&accelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+}
+
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+ 
+    if(type == WS_EVT_CONNECT){
+
+        Serial.println("Websocket client connection received");
+        client->text("Hello from ESP32 Server");
+    
+    } else if(type == WS_EVT_DISCONNECT){
+        Serial.println("Client disconnected");
+    
+    } else if(type == WS_EVT_DATA){
+
+        // do something based on the input
+
+        switch(data[0]){
+            case 'd':
+                // send data
+            case 'f':
+                // fire the catapult
+                allTime = micros();
+                Serial.println("catapult");
+        }
+    }
+}
+
+
 
 /*
     Main Loop
@@ -71,8 +116,7 @@ void setup() {
     if(!bno.begin())
     {
         /* There was a problem detecting the BNO055 ... check your connections */
-        WebSerial.println("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-        while(1);
+        Serial.println("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     }
 
     bno.setExtCrystalUse(true);
@@ -83,42 +127,38 @@ void setup() {
     dihedral_servo.attach(dihedral_pin);
 
     /*
-        Webserial
+        Web
     */
-    WiFi.mode(WIFI_STA);
+    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE);
     WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.printf("WiFi Failed!\n");
-        return;
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi..");
     }
-    Serial.print("IP Address: ");
+    
     Serial.println(WiFi.localIP());
-    // WebSerial is accessible at "<IP Address>/webserial" in browser
-    WebSerial.begin(&server);
-    WebSerial.msgCallback(recvMsg);
+
+    ws.onEvent(onWsEvent);
+    server.addHandler(&ws);
+    
     server.begin();
 }
 
 
 void loop() {
-    unsigned long tStart = micros();
+    Serial.println("HELOO");
 
-    // Get IMU data
-    sensors_event_t event;
-    bno.getEvent(&event);
+    tStart = micros();
 
-    WebSerial.println((float)event.orientation.x);
-    WebSerial.println((float)event.orientation.y);
-    WebSerial.println((float)event.orientation.z);
+    dihedral_pos += 1;
+    dihedral_servo.writeMicroseconds(dihedral_pos);
+    if (dihedral_pos == 2500) {
+        dihedral_pos = 1000;
+    }
 
-    sensors_event_t orientationData , accelData, gyroData;
+    sensor_data();
     
-    bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
-    bno.getEvent(&gyroData, Adafruit_BNO055::VECTOR_GYROSCOPE);
-    bno.getEvent(&accelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
-
-    WebSerial.println("hello");
-
     while ((micros() - tStart) < (BNO055_SAMPLERATE_DELAY_MS * 1000)) {
         //poll until the next sample is ready
     }
